2d168e8d3d5be072fbe4aae01ab0c14b55bf780d
Danny McCracken <dannymccracken13@yahoo.com>
2020-12-05 14:26:56 -0600

    added all functionality for products from ch5

---
+++ src/components/products/ProductProvider.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+
+/*
+    The context is imported and used by individual components
+    that need data
+*/
+export const ProductContext = React.createContext()
+
+/*
+ This component establishes what data can be used.
+ */
+export const ProductProvider = (props) => {
+    const [products, setProducts] = useState([])
+
+    const getProducts = () => {
+        return fetch("http://localhost:8088/products")
+            .then(res => res.json())
+            .then(setProducts)
+    }
+
+    const addProduct = product => {
+        return fetch("http://localhost:8088/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(product)
+        })
+            .then(getProducts)
+    }
+
+    return (
+        <ProductContext.Provider value={{
+            products, addProduct, getProducts
+        }}>
+            {props.children}
+        </ProductContext.Provider>
+    )
+}
